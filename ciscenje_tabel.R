d51b6b766868cf023558a287f5b4141d24643cc8
Neža Habjan <nezahabjan123@gmail.com>
2019-05-09 13:22:39 +0200

    prečistila tabelo 6

--- ciscenje_tabel.R
+++ ciscenje_tabel.R
@@ -30,6 +30,8 @@ data4 <- read_delim("U:/Podatki filmi/title.crew.tsv/data.tsv",
 
 data6 <- read_delim("U:/Podatki filmi/title.principals.tsv/data.tsv", 
                     "\t", escape_double = FALSE, trim_ws = TRUE)
+data6<-data6[,c(1,3,4)]
+data6<-subset(data6, category=='writer'|category=='actor'|category=='director')
 
 
 data7 <- read_delim("U:/Podatki filmi/title.ratings.tsv/data.tsv", 
