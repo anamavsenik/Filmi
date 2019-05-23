926bdc62fea8f215a5d5b70e10719b65162aa135
Ana Marija Kravanja <anamarija.kravanja@gmail.com>
2019-05-23 10:06:36 +0200

    dodala eno vrstico

--- ciscenje_tabel.R
+++ ciscenje_tabel.R
@@ -24,18 +24,18 @@ data3 <- data3[,c(3,6,8,9)]
 
 
 data4 <- read_delim("U:/Podatki filmi/title.crew.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE)
+                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=30)
 
 
 
 data6 <- read_delim("U:/Podatki filmi/title.principals.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE)
+                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=30)
 data6<-data6[,c(1,3,4)]
 data6<-subset(data6, category=='writer'|category=='actor'|category=='director')
 
 
 data7 <- read_delim("U:/Podatki filmi/title.ratings.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE)
+                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=30)
 data7 <- data7[,c(1,2)]
 
 
