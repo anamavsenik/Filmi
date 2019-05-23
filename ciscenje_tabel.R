8f68ed437f646eee7feb0fb23ce322f759bea4cd
Ana Marija Kravanja <anamarija.kravanja@gmail.com>
2019-05-23 13:20:10 +0200

    dodali sva tebele

--- ciscenje_tabel.R
+++ ciscenje_tabel.R
@@ -11,30 +11,30 @@ library(extrafont)
 library(tidyr)
 
 data1 <- read_delim("U:/Podatki filmi/name.basics.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE, na = "\\N", n_max=30)
+                    "\t", escape_double = FALSE, trim_ws = TRUE, na = "\\N", n_max=50000)
 stolpci<-c("id_os","Ime","leto_rojstva","leto_smrti","poklic","naslov_filma")
 names(data1)<-stolpci
 #ta tabela je ?e okej
 
 
-data3 <- read_delim("U:/Podatki filmi/title.basics.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max = 2000)
-data3 <- subset(data3,titleType=="movie")
+data33 <- read_delim("U:/Podatki filmi/title.basics.tsv/data.tsv", 
+                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max = 50000)
+data3 <- subset(data33,titleType=="movie")
 data3 <- data3[,c(3,6,8,9)]
 
 data4 <- read_delim("U:/Podatki filmi/title.crew.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=30)
+                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=50000)
 
 
 
 data6 <- read_delim("U:/Podatki filmi/title.principals.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=30)
+                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=50000)
 data6<-data6[,c(1,3,4)]
 data6<-subset(data6, category=='writer'|category=='actor'|category=='director')
 
 
 data7 <- read_delim("U:/Podatki filmi/title.ratings.tsv/data.tsv", 
-                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=30)
+                    "\t", escape_double = FALSE, trim_ws = TRUE,n_max=50000)
 data7 <- data7[,c(1,2)]
 
 
@@ -78,10 +78,24 @@ zanr_id <- c(1:length(imena_zanrov))
 vsi_zanri=data.frame(id=zanr_id, ime=imena_zanrov)
 vsi_zanri <- subset(vsi_zanri, vsi_zanri$ime!='\\N')
 
-#tabela, ki prikazuje filme in njihove žanre
+
+#tabela, ki prikazuje filme in njihove žanre, povezava - tabela IMA
 colnames(zanri) <- c("film", "ime_zanra")
-ima1 <- merge(zanri1, filmi, by = c(zanri$film, filmi$naslov) )
-ima2 <- merge(zanri1, vsi_zanri, by.x = "zvrst", by.y = "ime")
-ima <- ima2[, c(4,6)]
-colnames(ima) <- c("pesem_id", "zvrst_id")
-ima <- ima[with(ima, order(pesem_id)),]
+ima1 <- merge(zanri1, filmi, by.x = "film", by.y="naslov", all.x = TRUE)
+ima2 <- merge(ima1, vsi_zanri, by.x = "ime_zanra", by.y = "ime", all.x= TRUE)
+ima2[is.na(ima2)] <- "neznano" 
+ima<- ima2[,c(3,6)]
+colnames(ima) <- c("id_filma", "id_zanra")
+
+#tabela, ki povezuje osebe in filme, povezava - tabela NASTOPA
+#zopet preèistimo, enako kot pri žanrih:
+nastopi_v_filmih <- data.frame(ociscena1$Ime,ociscena1$naslov_filma)
+s<- strsplit(as.character(nastopi_v_filmih$ociscena1.naslov_filma), split = ",")
+nastopi_v_filmih1<- data.frame(oseba=rep(nastopi_v_filmih$ociscena1.Ime,sapply(s,length)),naslov=unlist(s))
+
+nastop1 <- merge(data33, nastopi_v_filmih1, by.x="tconst", by.y="naslov", all.x=TRUE)
+nastop2 <- merge(nastop1, filmi, by.x="primaryTitle", by.y="naslov", all.x=TRUE)
+nastop3 <- merge(nastop2, sodelujoci, by.x="oseba", by.y="ime", all.x=TRUE)
+nastopa <- nastop3[,c(11,14)]
+colnames(nastopa)<-c("id_filma", "id_osebe")
+
