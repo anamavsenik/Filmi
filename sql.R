8a1b24efdd3654c54c2f363ee4ef2bdcd0d6bf5f
Neža Habjan <nezahabjan123@gmail.com>
2019-05-27 13:34:55 +0200

    sql zacetek

--- sql.R
+++ sql.R
@@ -3,42 +3,76 @@ library(dplyr)
 library(dbplyr)
 library(RPostgreSQL)
 
-source("auth.R")
+source("auth_public.R",encoding="UTF-8")
+source("ciscenje_tabel.R", encoding="UTF-8")
 
 # Pove≈æemo se z gonilnikom za PostgreSQL
 drv <- dbDriver("PostgreSQL")
 
-# Uporabimo tryCatch,
-# da prisilimo prekinitev povezave v primeru napake
-tryCatch({
-  # Vzpostavimo povezavo
-  conn <- dbConnect(drv, dbname = db, host = host,
-                    user = user, password = password)
+# Funkcija za brisanje tabel
+delete_table <- function(){
+    # Uporabimo funkcijo tryCatch,
+    # da prisilimo prekinitev povezave v primeru napake
+    tryCatch({
+      # Vzpostavimo povezavo z bazo
+      conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+      
+      # »e tabela obstaja, jo zbriöemo, ter najprej zbriöemo tiste,
+      # ki se navezujejo na druge
+      dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS oseba CASCADE"))
+      dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS film CASCADE"))
+      dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS zanr CASCADE"))
+      dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS nastopa CASCADE"))
+      dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS nagrada CASCADE"))
+      dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS nosilec CASCADE"))
+      dbSendQuery(conn,build_sql("DROP TABLE IF EXISTS dobi CASCADE"))
+   
+    }, finally = {
+      dbDisconnect(conn)
+    })
+  }
   
-  # Poizvedbo zgradimo s funkcijo build_sql
-  # in izvedemo s funkcijo dbGetQuery
-  znesek <- 1000
-  t <- dbGetQuery(conn, build_sql("SELECT * FROM transakcija
-                                  WHERE znesek >", znesek, "
-                                  ORDER BY znesek, id", con=conn))
-  # Rezultat dobimo kot razpredelnico (data frame)
   
-  # Vstavimo ≈°e eno transakcijo
-  i <- round(runif(1, 1, nrow(t)))
-  print("Storniramo transkacijo:")
-  print(t[i,])
-  znesek <- -t[i, "znesek"]
-  racun <- t[i, "racun"]
-  opis <- paste("Storno:", t[i, "opis"])
+  #Funkcija, ki ustvari tabele
+  create_table <- function(){
+    # Uporabimo tryCatch (da se poveûemo in bazo in odveûemo)
+    # da prisilimo prekinitev povezave v primeru napake
+    tryCatch({
+      # Vzpostavimo povezavo
+      conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+      
+      # Glavne tabele
+      oseba <- dbSendQuery(conn, build_sql("CREATE TABLE oseba (
+                                               id INTEGER PRIMARY KEY, 
+                                               ime text NOT NULL)"))
+      
+      film <- dbSendQuery(conn, build_sql("CREATE TABLE film(
+                                           id INTEGER PRIMARY KEY,
+                                           naslov text NOT NULL)"))
+      
+      zanr <- dbSendQuery(conn, build_sql("CREATE TABLE zanr(
+                                           id INTEGER PRIMARY KEY,
+                                           ime text NOT NULL)"))
+      
+      nagrada <- dbSendQuery(conn, build_sql("CREATE TABLE nagrada(
+                                           id INTEGER PRIMARY KEY,
+                                           ime text NOT NULL,
+                                           leto_nagrade INTEGER NOT NULL,
+                                           kategorija text NOT NULL )"))
+      
+    
+      
+    }, finally = {
+      # Na koncu nujno prekinemo povezavo z bazo,
+      # saj preveË odprtih povezav ne smemo imeti
+      dbDisconnect(conn) #PREKINEMO POVEZAVO
+      # Koda v finally bloku se izvede, preden program konËa z napako
+    })
+  }
   
-  # Po≈°ljemo poizvedbo
-  dbSendQuery(conn, build_sql("INSERT INTO transakcija (znesek, racun, opis)
-                              VALUES (", znesek, ", ", racun, ", ", opis, ")", con=conn))
-}, finally = {
-  # Na koncu nujno prekinemo povezavo z bazo,
-  # saj preveƒç odprtih povezav ne smemo imeti
-  dbDisconnect(conn)
-  # Koda v finally bloku se izvede v vsakem primeru
-  # - bodisi ob koncu izvajanja try bloka,
-  # ali pa po tem, ko se ta konƒça z napako
-})
+  pravice()
+  delete_table()
+  create_table()
+  insert_data()
+  
+  #con <- src_postgres(dbname = db, host = host, user = user, password = password)  
