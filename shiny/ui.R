25470029dd7ba9c19cc9135fb9732fb9e3498ed4
anamkravanja <anamarija.kravanja@gmail.com>
2019-07-08 22:53:44 +0200

    začela z izdelavo spletne aplikacije

--- shiny/ui.R
+++ shiny/ui.R
@@ -1,17 +1,88 @@
 #source() klele bomo napisal od kje naj črpa podatke
+
 library(shiny)
 library(shinythemes)
 
-shinyUI(fluidPage(theme = shinytheme("cerulean"),
-                  
-                  
-                  titlePanel(" ")
-                  
-                  
-                    ))
-                  
-                  
-                  
 
-
-
+shinyUI(fluidPage(theme = "bootstrap.css",
+                  
+                
+                  headerPanel(
+                    h1( class = "title", img(src='film007.jpg', height=100, width=1000))
+                    
+                  ),
+                  
+                  column(3,offset = 4, titlePanel("Iskalnik filmov")), 
+                  
+                  
+                  mainPanel(
+                    tabsetPanel(
+                      
+                      # ZAVIHEK: Iskanje po naslovu
+                      tabPanel("Iskanje po naslovu",
+                               
+                               
+                               sidebarPanel(
+                                 textInput(inputId="film",label="Naslov filma","....")
+                               ),
+                               
+                               mainPanel(
+                                 tableOutput("film1451"),
+                                 textOutput("film245")
+                                 
+                               )),
+                      
+                      tabPanel("Iskanje po izvajalcu",
+                               sidebarPanel(
+                                 textInput(inputId="izvajalec", label="Izvajalec", "Ace of Base")
+                               ),
+                               
+                               mainPanel(
+                                 tableOutput("izvajalec55"),
+                                 textOutput("izvajalec2")
+                                 
+                               )
+                      ),
+                      
+                      # ZAVIHEK: Iskanje po albumu    
+                      tabPanel("Iskanje po albumu",
+                               sidebarPanel(
+                                 textInput(inputId="album",label="Album","Happy Nation")
+                               ),
+                               
+                               mainPanel(
+                                 tableOutput("album55"),
+                                 textOutput("album2")
+                               ) 
+                               
+                      ),
+                      
+                      # ZAVIHEK: Iskanje po zvrsti  
+                      tabPanel("Iskanje po zvrsti",
+                               sidebarPanel(
+                                 textInput(inputId="album",label="Album","Happy Nation")
+                               ),
+                                 
+                               mainPanel(
+                                 tableOutput("seznam1")
+                               )
+                               
+                      ),
+                      
+                      # ZAVIHEK: Iskanje po letu  
+                      tabPanel("Iskanje po letu",
+                               sidebarPanel(
+                                 sliderInput("leta",
+                                             "Leto skladbe:",
+                                             min = 1970,
+                                             max = 2015,
+                                             value = c(1980,2000))
+                               ),
+                               
+                               mainPanel(
+                                 tableOutput("tabelaleta")
+                               )
+                      )
+                      
+                      
+                    )    )    ) )    
