5f945a5b1eb436cb5211fe84603472653b719176
anamkravanja <anamarija.kravanja@gmail.com>
2019-07-08 19:06:15 +0200

    server

---
+++ shiny/ui.R
@@ -0,0 +1,17 @@
+#source() klele bomo napisal od kje naj črpa podatke
+library(shiny)
+library(shinythemes)
+
+shinyUI(fluidPage(theme = shinytheme("cerulean"),
+                  
+                  
+                  titlePanel(" ")
+                  
+                  
+                    ))
+                  
+                  
+                  
+
+
+
