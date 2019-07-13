3ce597cab84693506603279978cd21c5fd85d2ea
nezahabjan <nezahabjan123@gmail.com>
2019-07-13 15:41:48 +0200

    začetek vnosa gesla in imena uporabnikov

---
+++ serverFunctions.R
@@ -0,0 +1,89 @@
+library(bcrypt)
+library(shiny)
+library(shinydashboard)
+library(DT)
+library(shinyjs)
+sign.up.user <- function(username, pass){
+  # Return values:
+  # 1 ... success
+  # 0 ... error
+  # -10 ... username exists
+  success <- 0      # Boolean, if the insertion into the db was successful
+  
+  # hashing of password
+  pass <- hashpw(pass)
+  clan <- username
+  
+  useraccount <- data.frame(username = clan, password=pass)
+  uporabnikID <- NULL
+  
+  tryCatch({
+    drv <- dbDriver("PostgreSQL")
+    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+    userTable <- tbl(conn, "uporabnik")
+    # Pogledamo, ce je uporabnisko ime ze zasedeno
+    if(0 != dim((userTable %>% filter(username == clan) %>% collect()))[1]){
+      success <- -10
+    }
+    # Ce nam if stavek vrne True, potem v bazo uporabnik dodamo uporabnika z zaporedno stevilko, uporabniskim in geslom
+    sql_prijava <- build_sql("INSERT INTO uporabnik(username,hash)
+                             VALUES(",clan,",",pass,") RETURNING id", con = conn)
+    uporabnikID <- dbGetQuery(conn,sql_prijava)[[1]]
+    success <- 1
+  }, finally = {
+    dbDisconnect(conn)
+    return(list(success, uporabnikID))
+  })
+}
+
+sign.in.user <- function(username, pass){
+  # Return a list. In the first place is an indicator of success:
+  # 1 ... success
+  # 0 ... error
+  # -10 ... wrong username
+  # The second place represents the userid if the login info is correct,
+  # otherwise it's NULL
+  success <- 0
+  uporabnikID <- NULL
+  tryCatch({
+    drv <- dbDriver("PostgreSQL")
+    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+    userTable <- tbl(conn, "uporabnik")
+    obstoj <- 0
+    # obstoj = 0, ce username in geslo ne obstajata,  1 ce obstaja
+    uporabnik <- username
+    geslo <- pass
+    hashGesla <- (userTable %>% filter(username == uporabnik) %>% collect() %>% pull(hash))[[1]]
+    if(checkpw(geslo, hashGesla)){
+      obstoj <- 1
+    }
+    if(obstoj == 0){
+      success <- -10
+    }else{
+      uporabnikID <- (userTable %>% filter(username == uporabnik) %>%
+                        collect() %>% pull(id))[[1]]
+      success <- 1
+    }
+  },warning = function(w){
+    print(w)
+  },error = function(e){
+    print(e)
+  }, finally = {
+    dbDisconnect(conn)
+    return(list(success, uporabnikID))
+  })
+}
+
+pridobi.ime.uporabnika <- function(userID){
+  # Pridobi ime vpisanega glede na userID
+  tryCatch({
+    drv <- dbDriver("PostgreSQL")
+    conn <- dbConnect(drv, dbname = db, host = host, user = user, password = password)
+    sqlInput<- build_sql("SELECT username FROM uporabnik WHERE id=",userID, con = conn)
+    userid <- dbGetQuery(conn, sqlInput)
+  },finally = {
+    dbDisconnect(conn)
+    return(unname(unlist(userid)))
+  }
+  )
+}
--- shiny/server.R
+++ shiny/server.R
@@ -1,15 +1,132 @@
-library(shiny)
-library(dplyr)
-library(RPostgreSQL)
 
-source("../auth_public.R")
+source("../auth_public.r")
+source("serverFunctions.R")
 
-shinyServer(function(input, output) {
+#tukaj klici sql, ki se povezejo na ui.R
+
+shinyServer(function(input,output,session) {
   # Vzpostavimo povezavo
-  conn <- src_postgres(dbname = db, host = host,
-                       user = user, password = password)
+  drv <- dbDriver("PostgreSQL") 
+  conn <- dbConnect(drv, dbname = db, host = host,
+                    user = user, password = password)
+  userID <- reactiveVal()    # Placeholder za userID
+  loggedIn <- reactiveVal(FALSE)    # Placeholder za logout gumb oz vrednost gumba
   
-  # Povežemo se s tabelami, ki jih bomo rabili
+  dbGetQuery(conn, "SET CLIENT_ENCODING TO 'utf8'; SET NAMES 'utf8'")
+  
+  cancel.onSessionEnded <- session$onSessionEnded(function() {
+    dbDisconnect(conn) #ko zapremo shiny naj se povezava do baze zapre
+  })
+  output$signUpBOOL <- eventReactive(input$signup_btn, 1)
+  outputOptions(output, 'signUpBOOL', suspendWhenHidden=FALSE)  # Da omogoca skrivanje/odkrivanje
+  observeEvent(input$signup_btn, output$signUpBOOL <- eventReactive(input$signup_btn, 1))
+  
+  # Greyout of signin button
+  observeEvent(c(input$userName,input$password), {
+    shinyjs::toggleState("signin_btn", 
+                         all(c(input$userName, input$password)!=""))
+  })
+  
+  # Sign in protocol
+  observeEvent(input$signin_btn,
+               {signInReturn <- sign.in.user(input$userName, input$password)
+               if(signInReturn[[1]]==1){
+                 userID(signInReturn[[2]])
+                 output$signUpBOOL <- eventReactive(input$signin_btn, 2)
+                 loggedIn(TRUE)
+               }else if(signInReturn[[1]]==0){
+                 showModal(modalDialog(
+                   title = "Error during sign in",
+                   paste0("An error seems to have occured. Please try again."),
+                   easyClose = TRUE,
+                   footer = NULL
+                 ))
+               }else{
+                 showModal(modalDialog(
+                   title = "Wrong Username/Password",
+                   paste0("Username or/and password incorrect"),
+                   easyClose = TRUE,
+                   footer = NULL
+                 ))
+               }
+               })
+  
+  # Greyout of signup button
+  observeEvent(c(input$SignUpUserName, input$SignUpPassword), {
+    shinyjs::toggleState("signup_btnSignUp",
+                         all(c(input$SignUpUserName, input$SignUpPassword)!="")# & 
+                         # Preveri, ce samo latin characterji
+                         # !any(grepl("[^\x20-\x7F]",
+                         #         c(input$SignUpName, input$SignUpSurname, input$SignUpAddress, input$SignUpCity,
+                         #           input$SignUpCountry, input$SignUpEmso, input$SignUpMail, 
+                         #           input$SignUpUserName, input$SignUpPassword)))
+    )
+  })
+  
+  # Sign up protocol
+  observeEvent(input$signup_btnSignUp,
+               {
+                 if(any(grepl("[^\x20-\x7F]",
+                              c(input$SignUpUserName, input$SignUpPassword)))){
+                   success <- -1
+                 }else{
+                   signUpReturn <- sign.up.user(input$SignUpUserName, input$SignUpPassword)
+                   success <- signUpReturn[[1]]
+                 }
+                 if(success==1){
+                   showModal(modalDialog(
+                     title = "You have successfully signed up!",
+                     paste0("Now you can login as ",input$SignUpUserName,''),
+                     easyClose = TRUE,
+                     footer = NULL
+                   ))
+                   output$signUpBOOL <- eventReactive(input$signup_btnSignUp, 0) 
+                 }else if(success==-10){
+                   showModal(modalDialog(
+                     title = "Username conflict!",
+                     paste0("The username ",input$SignUpUserName,' is already taken. Please,
+                            chose a new one.'),
+                     easyClose = TRUE,
+                     footer = NULL
+                     ))
+                 }else if(success==-1){
+                   showModal(modalDialog(
+                     title = "Signup unsuccessful",
+                     paste0("Only Latin characters allowed"),
+                     easyClose = TRUE,
+                     footer = NULL
+                   ))
+                 }else{
+                   showModal(modalDialog(
+                     title = "Error during sign up",
+                     paste0("An error seems to have occured. Please try again."),
+                     easyClose = TRUE,
+                     footer = NULL
+                   ))
+                 }
+               })
+  
+  # Back button to sign in page
+  observeEvent(input$signup_btnBack, output$signUpBOOL <- eventReactive(input$signup_btnBack, 0))
+  
+  # Login/logout button in header
+  observeEvent(input$dashboardLogin, {
+    if(loggedIn()){
+      output$signUpBOOL <- eventReactive(input$signin_btn, 0)
+      userID <- reactiveVal()
+    }
+    loggedIn(ifelse(loggedIn(), FALSE, TRUE))
+  })
+  
+  output$logintext <- renderText({
+    if(loggedIn()) return("Logout here.")
+    return("Login here")
+  })
+  
+  output$dashboardLoggedUser <- renderText({
+    if(loggedIn()) return(paste("Welcome,", pridobi.ime.uporabnika(userID())))
+    return("")
+  })
   
   tbl.film <- tbl(conn, "film")
   tbl.zanr <- tbl(conn, "zanr")
@@ -21,9 +138,7 @@ shinyServer(function(input, output) {
   tbl.nosilec <- tbl(conn, "nosilec")
   tbl.dobi <- tbl(conn, "dobi")
   tbl.nagrada <- tbl(conn, "nagrada")
- 
+  
   
   
 })
-
-  
